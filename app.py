799cbb46676edb3cf1854846406280c0e7f70223
Lexander Thakur <143057892+LexanderThakur@users.noreply.github.com>
2025-07-06 17:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,8 @@ def generate():
 
 
 
+import os
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
